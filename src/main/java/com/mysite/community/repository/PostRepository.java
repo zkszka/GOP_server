446c7ad91a15c6a270cd84d5138438556705b332
zkszka <tenandten10@naver.com>
2024-07-31 03:11:08 +0900

    7/31번 커뮤니티 생성했습니다.

--- src/main/java/com/mysite/community/repository/PostRepository.java
+++ src/main/java/com/mysite/community/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.mysite.community.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mysite.community.entity.Post;
 
-public interface PostRepository extends JpaRepository<Post, Long>{
-
+public interface PostRepository extends JpaRepository<Post, Long> {
+	@Modifying
+	@Query("UPDATE Post p SET p.views = p.views + 1 WHERE p.id = :id")
+	void incrementViews(@Param("id") Long id);
 }
