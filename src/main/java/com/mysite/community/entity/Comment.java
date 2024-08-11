2a56c3e7f6ac3bd1a6d7f754680bad9d881ccbcc
zkszka <tenandten10@naver.com>
2024-08-11 18:50:28 +0900

    진짜 진짜 라스트임

--- src/main/java/com/mysite/community/entity/Comment.java
+++ src/main/java/com/mysite/community/entity/Comment.java
@@ -8,19 +8,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "content", nullable = false) // 실제 필드명에 맞춰서 수정
+    @Column(name = "content", nullable = false)
     private String content;
 
     @Column(nullable = false)
@@ -33,55 +34,10 @@ public class Comment {
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
-    public Comment() {
-        this.createdAt = LocalDateTime.now();
-    }
-
     public Comment(String content, String author, Post post) {
         this.content = content;
         this.author = author;
         this.createdAt = LocalDateTime.now();
-        this.post = post;
-    }
-
-    // 수동으로 추가된 메서드
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
         this.post = post;
     }
 }
--- src/main/java/com/mysite/community/entity/Post.java
+++ src/main/java/com/mysite/community/entity/Post.java
@@ -20,7 +20,7 @@ public class Post {
     private String content;
     private String author;
     private LocalDateTime createdAt;
-    private int views; 
+    private int views;
     private int comments;
     private String role;
 
@@ -28,70 +28,5 @@ public class Post {
         this.createdAt = LocalDateTime.now();
         this.views = 0;
         this.comments = 0;
-    }
-
-    // 수동으로 추가된 메서드
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
-
-    public int getComments() {
-        return comments;
-    }
-
-    public void setComments(int comments) {
-        this.comments = comments;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 }
