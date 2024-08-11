221e11cae41f9e2cc4640765f46e49ca5ab5b183
zkszka <tenandten10@naver.com>
2024-08-11 17:49:05 +0900

    진짜 마지막 수정

--- src/main/java/com/mysite/login/entity/Member.java
+++ src/main/java/com/mysite/login/entity/Member.java
@@ -1,12 +1,10 @@
 package com.mysite.login.entity;
 
 import java.sql.Timestamp;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,5 +27,54 @@ public class Member {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    // 수동으로 추가된 메서드
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
     }
 }
