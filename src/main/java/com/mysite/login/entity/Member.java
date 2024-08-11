d285a22a4311bbb646036c87ff546f58a101fa89
zkszka <tenandten10@naver.com>
2024-08-11 18:50:28 +0900

    진짜 진짜 라스트임

--- src/main/java/com/mysite/login/entity/Member.java
+++ src/main/java/com/mysite/login/entity/Member.java
@@ -17,6 +17,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String email;
     private String password;
@@ -27,54 +28,5 @@ public class Member {
         this.username = username;
         this.email = email;
         this.password = password;
-    }
-
-    // 수동으로 추가된 메서드
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Timestamp getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Timestamp createDate) {
-        this.createDate = createDate;
     }
 }
