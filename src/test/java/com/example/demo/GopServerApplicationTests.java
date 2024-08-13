84a26b09d423e2cab19a9ae10a49ec7312a00659
zkszka <tenandten10@naver.com>
2024-08-14 01:43:23 +0900

    테스트 없앰

--- src/test/java/com/example/demo/GopServerApplicationTests.java
+++ src/test/java/com/example/demo/GopServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GopServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
