1be2eda94afd95b2c04705d800088682963bc9d2
zkszka <tenandten10@naver.com>
2024-08-13 23:41:27 +0900

    찐막

--- src/main/java/com/mysite/login/entity/Member.java
+++ src/main/java/com/mysite/login/entity/Member.java
@@ -7,16 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,5 +19,64 @@ public class Member {
     private String role = "USER"; // 기본 role 설정
     private LocalDateTime createDate = LocalDateTime.now(); // 현재 시간으로 설정
 
-    // Lombok은 기본 생성자, 모든 필드를 포함한 생성자, getter, setter를 자동으로 생성합니다.
+    // 기본 생성자
+    public Member() {}
+
+    // 매개변수가 있는 생성자
+    public Member(String username, String email, String password, String role, LocalDateTime createDate) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createDate = createDate;
+    }
+
+    // getter와 setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
 }
