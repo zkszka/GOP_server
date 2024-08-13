5b30fcaaf005fc636bc021d80cd7e3ca02b85d72
zkszka <tenandten10@naver.com>
2024-08-14 01:59:46 +0900

    진짜 제발

--- src/test/java/com/example/demo/GopServerApplicationTests.java
+++ src/test/java/com/example/demo/GopServerApplicationTests.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class GopServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-		
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    void contextLoads() {
+        assertNotNull(context, "Application context should be loaded");
+    }
 }
