e6a87074493a0210ddacaddaae28b77a8d4137b2
zkszka <tenandten10@naver.com>
2024-08-16 02:44:52 +0900

    테스트 두번째 수정

--- src/test/java/com/mysite/test/GopServerApplicationTests.java
+++ src/test/java/com/mysite/test/GopServerApplicationTests.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class GopServerApplicationTests {
+public class GopServerApplicationTests {
 
     @Autowired
     private ApplicationContext context;
