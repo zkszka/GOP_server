8009f48b03596a1c18441c9609c31c40a4300b2c
zkszka <tenandten10@naver.com>
2024-09-19 21:28:27 +0900

    9/19일 클라우드타입으로 수정

--- src/main/java/com/mysite/WebConfig.java
+++ src/main/java/com/mysite/WebConfig.java
@@ -14,15 +14,15 @@ import java.util.Arrays;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-               .allowedOrigins("http://localhost:3000") // 클라이언트 origin 설정
-               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//               .allowedOrigins("http://localhost:3000") // 클라이언트 origin 설정
+//               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
